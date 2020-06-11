92182e7aeb94b3af7283dec14651031cd38fbbac
Tino <tinomuzambi@gmail.com>
2020-06-11 16:13:27 +0200

    Polishing form.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # Tino Muzambi
 # 2019/08/19 08:59
 # Remove contractions from your essays
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
+import os
 
 
-UPLOAD_FOLDER = './files'
-ALLOWED_EXTENSIONS = {'.txt'}
+UPLOAD_FOLDER = './files/'
+ALLOWED_EXTENSIONS = {'txt'}
 MAX_SIZE = 30000000
 
 
@@ -34,12 +35,28 @@ def upload_file():
     return render_template('upload.html')
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_files():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save('./' + UPLOAD_FOLDER + '/' + secure_filename(f.filename))
-        return 'file uploaded successfully'
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "Uploaded succesfully"
+        return render_template('upload.html')
 
 
 if __name__ == '__main__':
